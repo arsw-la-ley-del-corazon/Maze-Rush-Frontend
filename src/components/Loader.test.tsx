ac539f3133c94b633d7a4b070aa7d13b6cd0b141
Jeisson Sanchez <jeisson.sanchez-g@mail.escuelaing.edu.co>
2025-12-06 02:04:59 -0500

    Cobertura Sonar

--- src/components/Loader.test.tsx
+++ src/components/Loader.test.tsx
@@ -1,7 +1,6 @@
 // src/components/Loader.test.tsx
 import { describe, it, expect } from "vitest"
 import { render } from "@testing-library/react"
-import React from "react"
 import Loader from "./Loader"
 
 describe("Loader component", () => {
